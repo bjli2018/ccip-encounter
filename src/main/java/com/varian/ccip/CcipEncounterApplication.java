35b6199a866748c088c0becee903009e790ac85e
dkq6686 <Bob.Li@varian.com>
2019-10-29 17:17:00 +0800

    2019-10-29 17:16 提交

---
+++ src/main/java/com/varian/ccip/CcipEncounterApplication.java
@@ -0,0 +1,13 @@
+package com.varian.ccip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcipEncounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcipEncounterApplication.class, args);
+	}
+
+}
